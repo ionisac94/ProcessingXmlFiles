732b13b28e4cbe6cf01de3d9e664eb1e04ad5b98
Elitebook <ion.isac94@gmail.com>
2020-08-10 17:36:48 +0300

    updated README.md file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # SpringBoot app for parsing xml files.
+
+#### Local Environment Setup:
+##### Prerequisites:
+* Maven 3.6.2 +
+* Tomcat 9
+* Java 8
+
+##### Setup steps:
+
+
+* Update properties from `application.properties`
+        * refreshRate. Ex:`5000`
+
+* Build project ` mvn clean install -DskipTests`
+* Start application.
+    * Start using embedded server tomcat. -\> Start `main` method from `org.example.Main`
+    * Start using standalone server tomcat. -\> Copy `{ProjectFolder}/target/parser-*.war` to `{TomcatFolder}/webapps/`. Start Tomcat.
+* Verify. Run `GET http://localhost:8080/status`. expect `{"status": "UP"}`
+
+#### Unit Test Command:
+* `mvn clean test-compile test`
+
+# Additional Notes
--- src/main/java/org/example/helper/HelperParserXML.java
+++ src/main/java/org/example/helper/HelperParserXML.java
@@ -30,13 +30,14 @@ public class HelperParserXML {
 
 	public void parseXml() throws Exception {
 
-		File xmlFile = new File("C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\parserXML\\orders.xml");// only for test purpose..this path should no be hardcoded
+		File xmlFile = new File("C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\parserXML\\orders.xml");// only for test purpose..this path should no be hardcoded!!!
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		Document doc = dBuilder.parse(xmlFile);
 		doc.getDocumentElement().normalize();
 
 		String idNode = doc.getElementsByTagName("order").item(1).getAttributes().getNamedItem("ID").getNodeValue();
+		String substringNode = idNode.substring(0, idNode.length() - 2);
 
 		dbFactory.setNamespaceAware(true);
 		XPathFactory xfactory = XPathFactory.newInstance();
@@ -79,7 +80,7 @@ public class HelperParserXML {
 			Transformer transformer = transformerFactory.newTransformer();
 			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 			DOMSource source = new DOMSource(suppXml);
-			StreamResult result = new StreamResult(new File("C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\parserXML\\orders.xml" + supplier + idNode + ".xml"));
+			StreamResult result = new StreamResult(new File("C:\\Program Files\\Apache Software Foundation\\Tomcat 9.0\\parserXML\\" + supplier + substringNode + ".xml"));
 			transformer.transform(source, result);
 
 			LOGGER.info("Done for supplier: " + supplier);
