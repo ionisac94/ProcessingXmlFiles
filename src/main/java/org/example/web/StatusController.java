e602e387902590352b702331e15873b7f86fa108
Elitebook <ion.isac94@gmail.com>
2020-08-11 14:19:49 +0300

    updated logback.xml file

--- src/main/java/org/example/web/StatusController.java
+++ src/main/java/org/example/web/StatusController.java
@@ -1,11 +1,9 @@
 package org.example.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthComponent;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.health.Status;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
